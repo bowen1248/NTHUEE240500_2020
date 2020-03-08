46323d0dd4b7934c121caa6a3a2e59de49be149c
bowen1248 <bowenlee31@gmail.com>
2020-03-08 12:57:54 +0800

    done

---
+++ lab1/helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main (void) {
+    int i;
+    // print hello world for 9 times
+    for (i=1; i<10; ++i) {
+        printf("hello world %d times!\n", i); // hello
+    }
+    return 0;
+}
---
+++ mbed0/mbed-os-test-program/main.cpp
@@ -0,0 +1,12 @@
+#include "mbed.h"
+
+DigitalOut gpo(D0);
+DigitalOut led(LED_RED);
+
+int main() {
+   while (true) {
+       gpo = !gpo; // toggle pin
+       led = !led; // toggle led
+       wait(0.2f);
+   }
+}
