467b07a71973c03e14ab0809ae363b0f53f9df89
bowen1248 <bowenlee31@gmail.com>
2020-04-01 17:22:03 +0800

    testing

---
+++ exam01/main.cpp
@@ -0,0 +1,33 @@
+// uLCD-144-G2 basic text demo program for uLCD-4GL LCD driver library
+//
+#include "mbed.h"
+#include "uLCD_4DGL.h"
+
+PwmOut PWM1(D6);
+uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+
+int main()
+{
+    uLCD.background_color(0xFFFFFF); // white background
+    uLCD.textbackground_color(0xFFFFFF);
+    uLCD.cls();
+    // basic printf demo = 16 by 18 characters on screen
+    uLCD.color(BLUE);
+    uLCD.printf("\n107061113\n"); //Default Green on black text
+    uLCD.text_width(6); //4X size text
+    uLCD.text_height(6);
+    uLCD.color(GREEN);
+    uLCD.locate(2,2);
+    uLCD.filled_rectangle(32,32,96,96,GREEN);
+    PWM1.period(0.001);
+    while(1) {
+        for(float i = 0; i <= 1; i = i + 0.1) {
+            PWM1 = i;
+            wait(0.1);
+        }
+        for(float i = 1; i >= 0; i = i - 0.1) {
+            PWM1 = i;
+            wait(0.1);
+        }
+    }
+}
---
+++ hw2_digital_analog_io/main.cpp
@@ -0,0 +1,82 @@
+#include "mbed.h"
+#define max_voltage 3.3
+#define pi 3.1415926535897
+Serial pc( USBTX, USBRX );
+AnalogOut Aout(DAC0_OUT);
+AnalogIn Ain(A0);
+DigitalIn  Switch(SW3);
+DigitalOut redLED(LED1);
+DigitalOut greenLED(LED2);
+BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
+char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+
+float ADCdata[5000];
+float input_amplitude = 1.0;
+float offset = 1.0;
+int sampling_rate = 5000;
+int frequency = 0;
+int i;
+
+void DAC_output (int freq) {
+  int t = 0;
+  double sample = 0;
+  while (1) {
+    if (Switch == 0)
+      break;
+    sample = ((input_amplitude * (sin(2*pi*freq*t)) + offset) / max_voltage);
+    Aout.write(sample);
+    t = t + 0.0002;
+    pc.printf("%d\r\n",sample);
+    wait(0.0002);
+  }
+  Aout.write(0);
+  return;
+}
+
+void freq_display(int value) {
+  int dis_val = 0;
+  dis_val = value / 100;
+  display = table[dis_val];
+  wait(0.5);
+  dis_val = (value % 100) / 10;
+  display = table[dis_val];
+  wait(0.5);
+  dis_val = value % 10;
+  display = table[dis_val] + 0x80;
+  wait(0.5);
+  return;
+}
+
+int main() {
+  int count = 0;
+  Aout.write(0);
+  greenLED = 0;
+  redLED = 1;
+  while(1) {
+    count = 0;
+    for (i = 0; i < sampling_rate; i++) {
+      Aout = Ain;
+      ADCdata[i] = Ain;
+      wait(0.67/sampling_rate);
+    }
+    for (i = 0; i < sampling_rate; i++) {
+      if (ADCdata[i] <= (input_amplitude / max_voltage) / 2 && ADCdata[i + 1] >= (input_amplitude / max_voltage) / 2) {
+        count++;
+      }
+    }
+    frequency = count;
+    pc.printf("%d\r\n", frequency);
+    if( Switch == 0 ) {
+      greenLED = 1;
+      redLED = 0;
+      freq_display(frequency);
+    }
+    else {
+      greenLED = 0;
+      redLED = 1;
+      display = 0x00;
+      DAC_output(frequency);
+    }
+  }
+  return 0;
+}
