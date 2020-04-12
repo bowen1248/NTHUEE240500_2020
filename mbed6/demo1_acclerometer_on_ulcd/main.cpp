a15cb175210f8d086e9242f1c6c29bd9a87651f2
bowen1248 <bowenlee31@gmail.com>
2020-04-12 10:42:19 +0800

    mbed6 complete

---
+++ mbed6/demo1_acclerometer_on_ulcd/main.cpp
@@ -0,0 +1,89 @@
+#include "mbed.h"
+#include "TextLCD.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+
+#define UINT14_MAX 16383
+// FXOS8700CQ I2C address
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+
+// FXOS8700CQ internal register addresses
+#define FXOS8700Q_STATUS 0x00
+#define FXOS8700Q_OUT_X_MSB 0x01
+#define FXOS8700Q_OUT_Y_MSB 0x03
+#define FXOS8700Q_OUT_Z_MSB 0x05
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+#define FXOS8700Q_WHOAMI 0x0D
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+#define FXOS8700Q_CTRL_REG1 0x2A
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+#define FXOS8700Q_WHOAMI_VAL 0xC7
+
+// Host PC Communication channels
+Serial pc(USBTX, USBRX); // tx, rx
+
+// I2C Communication
+I2C i2c_lcd(D14, D15); // SDA, SCL
+I2C i2c(PTD9, PTD8);
+
+//TextLCD_SPI lcd(&spi_lcd, p8, TextLCD::LCD40x4);   // SPI bus, 74595 expander, CS pin, LCD Type
+TextLCD_I2C lcd(&i2c_lcd, 0x4E, TextLCD::LCD16x2);  // I2C bus, PCF8574 Slaveaddress, LCD Type
+//TextLCD_I2C lcd(&i2c_lcd, 0x42, TextLCD::LCD16x2, TextLCD::WS0010); // I2C bus, PCF8574 Slaveaddress, LCD Type, Device Type
+//TextLCD_SPI_N lcd(&spi_lcd, p8, p9);               // SPI bus, CS pin, RS pin, LCDType=LCD16x2, BL=NC, LCDTCtrl=ST7032_3V3
+//TextLCD_I2C_N lcd(&i2c_lcd, ST7032_SA, TextLCD::LCD16x2, NC, TextLCD::ST7032_3V3); // I2C bus, Slaveaddress, LCD Type, BL=NC, LCDTCtrl=ST7032_3V3
+
+int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
+void FXOS8700CQ_writeRegs(uint8_t * data, int len);
+
+int main() {
+    pc.baud(9600);
+    uint8_t who_am_i, data[2], res[6];
+    int16_t acc16;
+    float t[3];
+    // Enable the FXOS8700Q
+    FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
+    data[1] |= 0x01;
+    data[0] = FXOS8700Q_CTRL_REG1;
+    FXOS8700CQ_writeRegs(data, 2);
+    // Get the slave address
+    FXOS8700CQ_readRegs(FXOS8700Q_WHOAMI, &who_am_i, 1);
+    while (true) {
+    FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+    acc16 = (res[0] << 6) | (res[1] >> 2);
+    if (acc16 > UINT14_MAX/2)
+        acc16 -= UINT14_MAX;
+    t[0] = ((float)acc16) / 4096.0f;
+    acc16 = (res[2] << 6) | (res[3] >> 2);
+    if (acc16 > UINT14_MAX/2)
+        acc16 -= UINT14_MAX;
+    t[1] = ((float)acc16) / 4096.0f;
+    acc16 = (res[4] << 6) | (res[5] >> 2);
+    if (acc16 > UINT14_MAX/2)
+        acc16 -= UINT14_MAX;
+    t[2] = ((float)acc16) / 4096.0f;
+
+    // printout
+    lcd.printf("X=%6.3fY=%6.3f",t[0], t[1]);
+    lcd.locate(0, 1);
+    lcd.printf("Z=%6.3f", t[2]);
+    lcd.locate(0, 0);
+    wait(1);
+    }
+}
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+   char t = addr;
+   i2c.write(m_addr, &t, 1, true);
+   i2c.read(m_addr, (char *)data, len);
+}
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+   i2c.write(m_addr, (char *)data, len);
+}
