81128d072458beaa82661e9d3c1e5dea3355f65a
bowen1248 <bowenlee31@gmail.com>
2020-06-08 22:56:32 +0800

    hw4 complete

--- hw4_wifi_and_mqtt/main.cpp
+++ hw4_wifi_and_mqtt/main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include <stdlib.h>
 #include "fsl_port.h"
 #include "fsl_gpio.h"
 #include "mbed_rpc.h"
@@ -27,8 +28,11 @@
 #define FXOS8700Q_WHOAMI_VAL 0xC7
 
 I2C i2c( PTD9,PTD8);
-Serial pc(USBTX, USBRX);
+RawSerial pc(USBTX, USBRX);
+RawSerial xbee(D12, D11);
 Thread accel_thread;
+Thread xbee_thread;
+EventQueue xbee_queue;
 
 float sample_time[RECORD_TIMES];
 float x_acc[RECORD_TIMES], y_acc[RECORD_TIMES], z_acc[RECORD_TIMES];
@@ -36,96 +40,136 @@ int total_sample = 0;
 float current_time = 0;
 bool is_tilting = false;
 int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+int first_time_sample = 0;
+int get_rpc_times = 0;
 
+void xbee_rx_interrupt(void);
+void xbee_rx(void);
+void reply_messange(char *xbee_reply, char *messange);
+void check_addr(char *xbee_reply, char *messenger);
 void rpc_sample_num(Arguments *in, Reply *out);
-RPCFunction sample_num(&rpc_sample_num, "sample_num");
+RPCFunction sample_num(&rpc_sample_num, "get_sample_num");
 void read_accelero();
 void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
 void FXOS8700CQ_writeRegs(uint8_t * data, int len);
 
 int main() {
-    wifi = WiFiInterface::get_default_instance();
-    if (!wifi) {
-        printf("ERROR: No WiFiInterface found.\r\n");
-        return -1;
+    pc.baud(9600);
+    char xbee_reply[4];
+    // XBee setting
+    xbee.baud(9600);
+    xbee.printf("+++");
+    xbee_reply[0] = xbee.getc();
+    xbee_reply[1] = xbee.getc();
+    if(xbee_reply[0] == 'O' && xbee_reply[1] == 'K'){
+        pc.printf("enter AT mode.\r\n");
+        xbee_reply[0] = '\0';
+        xbee_reply[1] = '\0';
     }
-    printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
-    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-    if (ret != 0) {
-        printf("\nConnection error: %d\r\n", ret);
-        return -1;
-    }
-    NetworkInterface* net = wifi;
-    MQTTNetwork mqttNetwork(net);
-    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
-    //TODO: revise host to your ip
-    const char* host = "192.168.43.209";
-    printf("Connecting to TCP network...\r\n");
-    int rc = mqttNetwork.connect(host, 1883);
-    if (rc != 0) {
-        printf("Connection error.");
-        return -1;
-    }
-    printf("Successfully connected!\r\n");
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = 3;
-    data.clientID.cstring = "Mbed";
-    if ((rc = client.connect(data)) != 0){
-        printf("Fail to connect MQTT\r\n");
-    }
-    if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
-        printf("Fail to subscribe\r\n");
-    }
-    mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
-    Ticker ticker;
-    ticker.attach(mqtt_queue.event(&publish_message, &client), 0.5f);
-    int num = 0;
-    while (num != 5) {
-        client.yield(100);
-        ++num;
-    }
+    xbee.printf("ATMY 0x141\r\n");
+    reply_messange(xbee_reply, "setting MY : 0x141");
+
+    xbee.printf("ATDL 0x140\r\n");
+    reply_messange(xbee_reply, "setting DL : 0x140");
+
+    xbee.printf("ATID 0x3\r\n");
+    reply_messange(xbee_reply, "setting PAN ID : 0x3");
+
+    xbee.printf("ATWR\r\n");
+    reply_messange(xbee_reply, "write config");
+
+    xbee.printf("ATMY\r\n");
+    check_addr(xbee_reply, "MY");
+
+    xbee.printf("ATDL\r\n");
+    check_addr(xbee_reply, "DL");
+
+    xbee.printf("ATCN\r\n");
+    reply_messange(xbee_reply, "exit AT mode");
+
+    xbee.getc();
+    // start
+    pc.printf("start\r\n");
+    xbee_thread.start(callback(&xbee_queue, &EventQueue::dispatch_forever));
+
+    // Setup a serial interrupt function of receiving data from xbee
+    xbee.attach(xbee_rx_interrupt, Serial::RxIrq);
+
     accel_thread.start(read_accelero);
-    while (1) {
-        pc.printf("hello\r\n");
-        wait(5);
-    }
-}
-void messageArrived(MQTT::MessageData& md) {
-    MQTT::Message &message = md.message;
-    char msg[300];
-    sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
-    printf(msg);
-    wait_ms(1000);
-    char payload[300];
-    sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
-    printf(payload);
-    ++arrivedcount;
+    while (1) {wait(1);}
+    return 0;
 }
 
-void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
-    while(1) {
-        message_num++;
-        MQTT::Message message;
-        char buff[100];
-        sprintf(buff, "FXOS8700Q ACC: X=%1.4f Y=%1.4f Z=%1.4f #%d\r\n",\
-                  t[0], t[1], t[2], message_num);
-        message.qos = MQTT::QOS0;
-        message.retained = false;
-        message.dup = false;
-        message.payload = (void*) buff;
-        message.payloadlen = strlen(buff) + 1;
-        int rc = client->publish(topic, message);
-        printf("rc:  %d\r\n", rc);
-        printf("Puslish message: %s\r\n", buff);
+void xbee_rx_interrupt(void) {
+    xbee.attach(NULL, Serial::RxIrq); // detach interrupt
+    xbee_queue.call(&xbee_rx);
+}
+
+void xbee_rx(void) {
+    char buf[100] = {0};
+    char outbuf[100] = {0};
+    while(xbee.readable()){
+        for (int i=0; ; i++) {
+            char recv = xbee.getc();
+            if (recv == '\r' || recv == '\n') {
+                break;
+            }
+            buf[i] = recv;
+        }
+        RPC::call(buf, outbuf);
     }
+    xbee.attach(xbee_rx_interrupt, Serial::RxIrq); // reattach interrupt
+}
+
+void reply_messange(char *xbee_reply, char *messange){
+  xbee_reply[0] = xbee.getc();
+  xbee_reply[1] = xbee.getc();
+  xbee_reply[2] = xbee.getc();
+  if(xbee_reply[1] == 'O' && xbee_reply[2] == 'K'){
+   pc.printf("%s\r\n", messange);
+   xbee_reply[0] = '\0';
+   xbee_reply[1] = '\0';
+   xbee_reply[2] = '\0';
+  }
+}
+
+
+void check_addr(char *xbee_reply, char *messenger){
+  xbee_reply[0] = xbee.getc();
+  xbee_reply[1] = xbee.getc();
+  xbee_reply[2] = xbee.getc();
+  xbee_reply[3] = xbee.getc();
+  pc.printf("%s = %c%c%c\r\n", messenger, xbee_reply[1], xbee_reply[2], xbee_reply[3]);
+  xbee_reply[0] = '\0';
+  xbee_reply[1] = '\0';
+  xbee_reply[2] = '\0';
+  xbee_reply[3] = '\0';
 }
 
 void rpc_sample_num (Arguments *in, Reply *out)  {
-    static prev_sec_sample = 0;
+    static int prev_sec_sample = 0;
     int this_sec_sample = 0;
+    if (get_rpc_times == 0) 
+        first_time_sample = total_sample;
     this_sec_sample = total_sample - prev_sec_sample;
     prev_sec_sample = total_sample;
-    pc.printf("%d/r/n", this_sec_sample);
+    //pc.printf("%d\r\n", this_sec_sample);
+    xbee.printf("%d\r\n", this_sec_sample);
+    get_rpc_times++;
+    if (get_rpc_times >= 21) {
+        accel_thread.terminate();
+        for (int i = first_time_sample; i < first_time_sample + 40; i++) {
+            xbee.printf("%f\n", x_acc[i]);
+            pc.printf("%f\r\n", x_acc[i]);
+            wait(0.1);
+            xbee.printf("%f\n", y_acc[i]);
+            pc.printf("%f\r\n", y_acc[i]);
+            wait(0.1);
+            xbee.printf("%f\n", z_acc[i]);
+            pc.printf("%f\r\n", z_acc[i]);
+            wait(0.1);
+        }
+    }
 }
 void read_accelero() {
     uint8_t who_am_i, data[2], res[6];
@@ -142,32 +186,25 @@ void read_accelero() {
         acc16 = (res[0] << 6) | (res[1] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        x_acc[total_sample] = ((float)acc16) / 4096.0f;
+        x_acc[total_sample] = acc16 / 4096.0;
         acc16 = (res[2] << 6) | (res[3] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        y_acc[total_sample] = ((float)acc16) / 4096.0f;
+        y_acc[total_sample] = acc16 / 4096.0;
         acc16 = (res[4] << 6) | (res[5] >> 2);
         if (acc16 > UINT14_MAX/2)
             acc16 -= UINT14_MAX;
-        z_acc[total_sample] = ((float)acc16) / 4096.0f;
-        if ((z_acc[total_sample] / sqrt(x_acc[total_sample] * x_acc[total_sample]
+        z_acc[total_sample] = acc16 / 4096.0;
+        /* if ((z_acc[total_sample] / sqrt(x_acc[total_sample] * x_acc[total_sample]
              + y_acc[total_sample] * y_acc[total_sample] + z_acc[total_sample] * z_acc[total_sample])) < 0.7071)
             is_tilting = true;
-        else is_tilting = false;
-        sample_time[total_sample] = current_time;
-        printf("#%d %d %f ACC: X=%1.4f Y=%1.4f Z=%1.4f\r\n",\
-            total_sample, is_tilting, sample_time[total_sample], x_acc[total_sample], y_acc[total_sample], z_acc[total_sample]);
+        else is_tilting = false; */
+        // sample_time[total_sample] = current_time;
+        //printf("#%d ACC: X=%d Y=%d Z=%d\r\n",\
+          //  total_sample, x_acc[total_sample], y_acc[total_sample], z_acc[total_sample]);
         total_sample++;
-        if (is_tilting) {
-            accel_thread.wait(100);
-            current_time = current_time + 0.1;
-            
-        }
-        else {
-            accel_thread.wait(500);
-            current_time = current_time + 0.5;
-        }
+        accel_thread.wait(500);
+        current_time = current_time + 0.5;
     }
 }
 void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
